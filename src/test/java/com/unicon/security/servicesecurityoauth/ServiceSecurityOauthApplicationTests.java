ea5e64eabf7fcf940d51c470eb5fcb8b586e1e0b
Antonio Crispin <antonio.crispin@gmail.com>
2019-11-29 09:28:16 -0500

    first commit

---
+++ src/test/java/com/unicon/security/servicesecurityoauth/ServiceSecurityOauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unicon.security.servicesecurityoauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceSecurityOauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
